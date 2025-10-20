8d83839737ca2e44cf236fc3d6b4e7d1bcab50c5
JJ Kavelaars <JJ.Kavelaars@nrc-cnrc.gc.ca>
2025-10-20 11:54:54 -0700

    Use the skaha startup script to ensure the correct Jupyter starts

--- science-containers/Dockerfiles/lsst-science-platform/src/startup.sh
+++ science-containers/Dockerfiles/lsst-science-platform/src/startup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -a
-SHELL=bash
-CPU_LIMIT=${OMP_NUM_THREADS}
+export SHELL="bash"
+. /etc/profile
+CPU_LIMIT="${OMP_NUM_THREADS}"
 export RUNNING_INSIDE_JUPYTERLAB="TRUE"
 export EXTERNAL_INSTANCE_URL="https://data.lsst.cloud"
 export TMPDIR="/tmp"
-export FIREFLY_URL="localhost:8888/firefly"
-export JUPYTER_PREFER_ENV_PATH="no"
-export SHELL="bash"
+
+# use the cadc remote butler which is connected to Storage Inventory 
+export DAF_BUTLER_SERVER_GAFAELFAWR_URL=DISABLED
+export DAF_BUTLER_SERVER_AUTHENTICATION=cadc
 [ -f ~/.ssl/cadcproxy.pem ] && export CADC_TOKEN=$(curl -E ~/.ssl/cadcproxy.pem "https://ws-cadc.canfar.net/ac/authorize?response_type=token")
-. /etc/profile
-exec $@
+
+exec bash $@
