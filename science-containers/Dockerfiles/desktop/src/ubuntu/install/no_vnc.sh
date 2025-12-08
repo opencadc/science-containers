357f0a9083e30a5c714f0976f8a79337dd2edf52
Dustin Jenkins <djenkins.cadc@gmail.com>
2025-12-08 14:58:43 -0800

    fix: desktop build updates

--- science-containers/Dockerfiles/desktop/src/ubuntu/install/no_vnc.sh
+++ science-containers/Dockerfiles/desktop/src/ubuntu/install/no_vnc.sh
@@ -13,6 +13,10 @@ wget -qO- https://github.com/novnc/noVNC/archive/v1.6.0.tar.gz | tar xz --strip 
 # use older version of websockify to prevent hanging connections on offline containers, see https://github.com/ConSol/docker-headless-vnc-container/issues/50
 # wget -qO- https://github.com/novnc/websockify/archive/v0.6.1.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME/utils/websockify
 wget -qO- https://github.com/novnc/websockify/archive/v0.13.0.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME/utils/websockify
-chmod +x -v $NO_VNC_HOME/utils/*.sh
+
+if [ -d "$NO_VNC_HOME/utils" ]; then
+    chmod +x -v $NO_VNC_HOME/utils/*.sh
+fi
+
 ## create index.html to forward automatically to `vnc_lite.html`
 ln -s $NO_VNC_HOME/vnc_lite.html $NO_VNC_HOME/index.html
