1ef0f0818c296a79144273ce5699820d49020503
Dustin Jenkins <djenkins.cadc@gmail.com>
2025-12-15 14:41:35 -0800

    fix: add dbus to install

--- science-containers/Dockerfiles/desktop/src/ubuntu/install/xfce_ui.sh
+++ science-containers/Dockerfiles/desktop/src/ubuntu/install/xfce_ui.sh
@@ -6,7 +6,7 @@ echo "Install Xfce4 UI components"
 rm -rf /var/cache/apt/archives/*
 rm -rf /var/lib/apt/lists/*
 apt update
-apt install -y supervisor xfce4 xfce4-terminal
+apt install -y dbus-x11 supervisor xfce4 xfce4-terminal
 apt purge -y pm-utils xscreensaver*
 apt clean -y
 rm -rf /var/lib/apt/lists/*
